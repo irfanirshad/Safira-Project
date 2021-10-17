a3b4b5fd86e80895712601d247d48c70c83475f4
irfanirshad003 <84003624+irfanirshad@users.noreply.github.com>
2021-10-18 04:35:58 +0530

    Added try except statement that force opens notepad if doesnt exists

--- modules/notepad.CODE-Injector.py
+++ modules/notepad.CODE-Injector.py
@@ -1,15 +1,31 @@
-import os
-import subprocess
 import pymem
-s
+import subprocess
+s  # to prevent accidental execution of our program inside our env
+
+'''
+A simple application opener and a python code injector
+that we can leverage to our advantage by inserting malicious code
+if we wished to 
 
 
-mem = pymem.Pymem("notepad.exe")
+'''
+
+
+# We try to read the memory of the NotePad's process
+try:
+    mem = pymem.Pymem("notepad.exe")
+except:
+    # If not found we force open it ourselves
+    subprocess.Popen("notepad.exe")
+    mem = pymem.Pymem("notepad.exe")
+
 mem.inject_python_interpreter()
 
+# Here lies the code in python we wish to inject inside the notepad
 code = """
     import tkinter as tk
 
-    win 
+    win = tk.Tk()
+    win.mainloop()
 
 """
